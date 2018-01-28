b1de036480d42fd204c2c4bbfbd9336bb1b15117
yukinonny02 <yukinonny02@gmail.com>
2018-01-28 16:19:56 +0700

    Initial commit

---
+++ src/ooplab/MyfirstJAVA.java
@@ -0,0 +1,11 @@
+package ooplab;
+
+public class MyfirstJAVA {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl,I'm yu");
+        System.out.println("Information systsm");
+        System.out.println("Faculty of Mangement Technology");
+        System.out.println("rmutsv");
+
+    }
+}
