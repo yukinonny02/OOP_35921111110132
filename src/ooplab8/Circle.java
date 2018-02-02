4f6cd2555b4e85a8a8be76d3f43b8771854312d6
LAB_COM-MT_1 <yukinonny02@gmail.com>
2018-02-02 11:33:11 +0700

    Circle

---
+++ src/ooplab8/Circle.java
@@ -0,0 +1,22 @@
+package ooplab8;
+
+public class Circle implements Shape {
+    private double r;
+
+    public Circle(double r) {
+        this.r = r;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    @Override
+    public double findArea() {
+        return 3.14*(r*r);
+    }
+}//class
