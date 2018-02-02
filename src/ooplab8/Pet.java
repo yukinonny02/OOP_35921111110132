128edd835e05eb4d6615e62631bf666e34a33bce
LAB_COM-MT_1 <yukinonny02@gmail.com>
2018-02-02 11:19:31 +0700

    Pet

---
+++ src/ooplab8/Pet.java
@@ -0,0 +1,29 @@
+package ooplab8;
+
+public abstract class Pet {
+    private String name;
+    private int age;
+
+    public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    //abstract
+    public abstract void  makeNoise();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}//clas
