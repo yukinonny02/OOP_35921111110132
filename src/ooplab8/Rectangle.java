07b2bab07645a98e405d0ff872bd6d28f9a2a043
LAB_COM-MT_1 <yukinonny02@gmail.com>
2018-02-02 11:32:12 +0700

    Rectangle

---
+++ src/ooplab8/Rectangle.java
@@ -0,0 +1,32 @@
+package ooplab8;
+
+public class Rectangle implements Shape{
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double findArea() {
+        return width*length;
+    }
+}//clas
